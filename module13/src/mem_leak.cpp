743138b3bf0aa82be1cebdd1541f1858bf30436a
Konstantin Latuta <konstantin.latuta@gmail.com>
2023-06-05 19:35:04 +0600

    Update mem_leak.cpp
    Добавил код

--- module13/src/mem_leak.cpp
+++ module13/src/mem_leak.cpp
@@ -1,1 +1,31 @@
-asd
+// Пример утечки памяти в Си++
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void f(){
+    int* ptr = new int[1000000000];
+    // delete [] ptr; // освобождаем память за собой...
+    // delete ptr; // неопределенное поведение
+}
+
+int main()
+{
+    int count = 0;
+    while(1){
+        cout << ++count << endl;
+        f(); // каждый вызов этой функции выделяет миллиард интов на куче и забывает про них
+    }
+}
+
+/* После выделения 35183 миллиардов int-ов ~ , на куче кончилось место (в виртуальной памяти).
+...
+35183
+35184
+terminate called after throwing an instance of 'std::bad_alloc'
+  what():  std::bad_alloc
+Aborted (core dumped)
+*/
